0bdf8c6cb5bdf918d6f7662795b310bfdd30eb35
b3ingsahil007 <chhipasahil163@gmail.com>
2026-01-08 18:57:59 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -223,6 +223,10 @@ process.on('uncaughtException', (err) => {
     console.error('Uncaught Exception:', err);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
